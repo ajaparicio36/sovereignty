c7d440f10c0003b381463a72e996ef0dd5217e5a
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2025-04-17 18:40:40 +0800

    Initial commit

---
+++ src/main/java/com/tatayless/sovereignty/Sovereignty.java
@@ -0,0 +1,17 @@
+package com.tatayless.sovereignty;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Sovereignty extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getLogger().info("Sovereignty has been enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        getLogger().info("Sovereignty has been disabled!");
+    }
+}
